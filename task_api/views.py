eebeb309bb251909012efa3a4ad33b1cf0b52890
Marcin <marcin.gralka123@gmail.com>
2024-04-29 02:59:02 +0200

    Add error handling

--- task_api/views.py
+++ task_api/views.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 GITHUB_API_URL = 'https://api.github.com/search/repositories'
 
@@ -21,18 +21,21 @@ def search_repositories(request):
         }
         response = requests.get(GITHUB_API_URL, params=params, headers=headers)
 
-    data = response.json()
-    repositories = []
+    if response.status_code == 200:
+        data = response.json()
+        repositories = []
 
-    for repository in data.get('items', []):
-        repositories.append(
-            {
-                'name': repository['name'],
-                'owner_login': repository['owner']['login'],
-                'url': repository['html_url']
-            }
-        )
+        for repository in data.get('items', []):
+            repositories.append(
+                {
+                    'name': repository['name'],
+                    'owner_login': repository['owner']['login'],
+                    'url': repository['html_url']
+                }
+            )
 
-    json_repositories = json.dumps({'keyword': keyword, 'repositories': repositories}, indent=4)
+        json_repositories = json.dumps({'keyword': keyword, 'repositories': repositories}, indent=4)
 
-    return HttpResponse(json_repositories, content_type='application/json')
+        return HttpResponse(json_repositories, content_type='application/json')
+    else:
+        return JsonResponse({'error': 'Failed to retrieve data from Github API'}, status=response.status_code)
