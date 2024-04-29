0925571056aaa7c1cd07d72cc6a16e748ada219e
Marcin <marcin.gralka123@gmail.com>
2024-04-29 04:00:40 +0200

    Test for failed response

--- task_api/tests.py
+++ task_api/tests.py
@@ -30,4 +30,13 @@ class SearchTestCase(unittest.TestCase):
         assert data['keyword'] == 'test'
         assert data['repositories'][0] == {'name': 'test-repo', 'owner_login': 'test-user',
                                            'url': 'https://github.com/test-user/test-repo'}
-    
+
+    @patch('requests.get')
+    def test_error_response(self, mock_get):
+        mock_response = mock.Mock()
+        mock_response.status_code = 403
+        mock_get.return_value = mock_response
+
+        response = Client().get('/github/search/?keyword=test')
+        assert response.status_code == 403
+        assert response.json() == {'error': 'Failed to retrieve data from Github API'}
