0577eb4382754acd8ae63cf841edf0e081b14cab
Marcin <marcin.gralka123@gmail.com>
2024-04-29 03:34:09 +0200

    Test for missing keyword

--- task_api/tests.py
+++ task_api/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import json
+import unittest
 
-# Create your tests here.
+import requests
+from unittest.mock import patch
+from django.test import Client
+from .views import search_repositories
+
+class SearchTestCase(unittest.TestCase):
+    @patch('requests.get')
+    def test_missing_keyword(self, mock_get):
+        response = Client().get('/github/search/')
+        assert response.status_code == 400
+        assert response.json() == {'error': 'Keyword parameter is required'}
+
+    
