5230f22afd33319347c2e194c41c6fa7dccd9dbd
Marcin <marcin.gralka123@gmail.com>
2024-04-29 03:45:46 +0200

    Test for successful response

--- task_api/tests.py
+++ task_api/tests.py
@@ -1,10 +1,8 @@
-import json
 import unittest
-
-import requests
+from unittest import mock
 from unittest.mock import patch
 from django.test import Client
-from .views import search_repositories
+import json
 
 class SearchTestCase(unittest.TestCase):
     @patch('requests.get')
@@ -13,4 +11,23 @@ class SearchTestCase(unittest.TestCase):
         assert response.status_code == 400
         assert response.json() == {'error': 'Keyword parameter is required'}
 
+    @patch('requests.get')
+    def test_successful_request(self, mock_get):
+        mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            'items': [
+                {'name': 'test-repo', 'owner': {'login': 'test-user'},
+                 'html_url': 'https://github.com/test-user/test-repo'},
+            ]
+        }
+        mock_get.return_value = mock_response
+
+        response = Client().get('/github/search/?keyword=test')
+        assert response.status_code == 200
+        data = json.loads(response.content)
+
+        assert data['keyword'] == 'test'
+        assert data['repositories'][0] == {'name': 'test-repo', 'owner_login': 'test-user',
+                                           'url': 'https://github.com/test-user/test-repo'}
     
